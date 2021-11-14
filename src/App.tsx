233ce613189773891fe28a3c57cc37e952637dfb
darckstalker <bokarev_87@mail.ru>
2021-11-14 10:04:35 +0300

    Initial version

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {FC, useContext, useEffect, useState} from 'react';
+import { observer } from 'mobx-react-lite';
+import { Context } from '.';
 import './App.css';
+import LoginForm from './components/LoginForm';
+import { IUser } from './models/IUser';
+import UserService from './services/UserService';
 
-function App() {
+const  App: FC = () => {
+  const {store} = useContext(Context);
+  const [users, setUsers] = useState<IUser[]>([]);
+
+  useEffect(() => {
+    if (localStorage.getItem('token')) {
+      store.checkAuth();
+    }
+  }, [])
+
+  async function getUsers() {
+    try {
+      const response = await UserService.fetchUsers();
+      setUsers(response.data);
+    } catch (e) {
+      if (e){
+        console.log(e);
+      }
+    }
+  }
+
+  if (store.isLoading) {
+    return <div>Загрузка...</div>
+  }
+
+  if (!store.isAuth) {
+    return (
+      <>
+        <LoginForm />
+        <div>
+          <button onClick={() => getUsers()}>Get all users</button>
+        </div>
+        {
+          users.map(user => <div key={user.email}>{user.email}</div>)
+        }
+      </>
+    )
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{store.isAuth ? `User authorized ${store.user.email}` : 'Authorize'}</h1>
+      <h1>{store.user.isActivated ? 'Account eMail verified' : 'You need to verify your account'}</h1>
+      <button onClick={() => store.logout()}>Logout</button>
+      <div>
+        <button onClick={() => getUsers()}>Get all users</button>
+      </div>
+      {
+        users.map(user => <div key={user.email}>{user.email}</div>)
+      }
     </div>
   );
 }
 
-export default App;
+export default observer(App);
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { createContext } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Store from './store/store';
+import './index.css';
+
+interface State {
+  store: Store,
+}
+
+const store = new Store();
+
+export const Context = createContext<State>({
+  store,
+})
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Context.Provider value={{store}}>
     <App />
-  </React.StrictMode>,
+  </Context.Provider>,
   document.getElementById('root')
 );
 
